2e67fc2bb366fb70149ae713e50987210a66355e
Chybozz <martinsonuchukwuchimdinyerem.com>
2025-04-08 14:37:30 +0100

    Updates Credentials

--- db.py
+++ db.py
@@ -3,10 +3,10 @@ import mysql.connector
 
 def get_db_connection():
     return mysql.connector.connect(
-        user=os.environ['DB_USER'],  # Replace with your MySQL username
-        password=os.environ['DB_PASS'],  # Replace with your MySQL password
-        database=os.environ['DB_NAME'],  # Replace with your database name
-        unix_socket=f"/cloudsql/{os.environ['DB_HOST']}"
+        user=os.environ.get('DB_USER'),  # Replace with your MySQL username
+        password=os.environ.get('DB_PASS'),  # Replace with your MySQL password
+        database=os.environ.get('DB_NAME'),  # Replace with your database name
+        unix_socket=f"/cloudsql/{os.environ.get('DB_HOST')}"
     )
 
 # host='localhost',
