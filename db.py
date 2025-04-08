c8243562edf20c438bd6d1f0586e462f92cd24e3
Chybozz <martinsonuchukwuchimdinyerem.com>
2025-04-08 13:38:32 +0100

    Updated Credentials

--- db.py
+++ db.py
@@ -1,11 +1,13 @@
+import os
 import mysql.connector
 
 def get_db_connection():
     return mysql.connector.connect(
         host='localhost',  # stays localhost when using Cloud SQL Auth Proxy or when deploying with Cloud SQL integrated
-        user='stallion_user',  # Replace with your MySQL username
-        password='stallionroutes123',  # Replace with your MySQL password
-        database='stallion_data'  # Replace with your database name
+        user=os.environ['DB_USER'],  # Replace with your MySQL username
+        password=os.environ['DB_PASS'],  # Replace with your MySQL password
+        database=os.environ['DB_NAME'],  # Replace with your database name
+        unix_socket=f"/cloudsql/{os.environ['DB_HOST']}"
     )
 
 # host='localhost',
