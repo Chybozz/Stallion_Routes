f143092d36ff8064a8072f9aa755c8273eebaef7
Chybozz <martinsonuchukwuchimdinyerem.com>
2025-04-08 17:03:44 +0100

    Updated

--- db.py
+++ db.py
@@ -1,13 +1,18 @@
 import os
 import mysql.connector
+from mysql.connector import Error
 
 def get_db_connection():
-    return mysql.connector.connect(
-        user=os.environ.get('DB_USER'),  # Replace with your MySQL username
-        password=os.environ.get('DB_PASS'),  # Replace with your MySQL password
-        database=os.environ.get('DB_NAME'),  # Replace with your database name
-        unix_socket=f"/cloudsql/{os.environ.get('DB_CONNECTION_NAME')}"
-    )
+    try:
+        return mysql.connector.connect(
+            user=os.environ.get('DB_USER'),  # Replace with your MySQL username
+            password=os.environ.get('DB_PASS'),  # Replace with your MySQL password
+            database=os.environ.get('DB_NAME'),  # Replace with your database name
+            unix_socket=f"/cloudsql/{os.environ.get('DB_CONNECTION_NAME')}"
+        )
+    except Error as err:
+        print(f"Error: {err}")
+        return None
 
 # host='localhost',
 # user='root',
