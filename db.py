835bc1e1c6f747c97967b4d5f8e77adccf7a8ed6
Chybozz <martinsonuchukwuchimdinyerem.com>
2025-04-08 14:10:30 +0100

    Updated Credentials

--- db.py
+++ db.py
@@ -3,7 +3,6 @@ import mysql.connector
 
 def get_db_connection():
     return mysql.connector.connect(
-        host='localhost',  # stays localhost when using Cloud SQL Auth Proxy or when deploying with Cloud SQL integrated
         user=os.environ['DB_USER'],  # Replace with your MySQL username
         password=os.environ['DB_PASS'],  # Replace with your MySQL password
         database=os.environ['DB_NAME'],  # Replace with your database name
