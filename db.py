a604d81e67902b63edc729b49709f14442ca1ead
Chybozz <martinsonuchukwuchimdinyerem.com>
2025-04-09 12:09:49 +0100

    Updates

--- db.py
+++ db.py
@@ -4,12 +4,14 @@ from mysql.connector import Error
 
 def get_db_connection():
     try:
-        return mysql.connector.connect(
-            user=os.environ.get('DB_USER'),  # Replace with your MySQL username
-            password=os.environ.get('DB_PASS'),  # Replace with your MySQL password
-            database=os.environ.get('DB_NAME'),  # Replace with your database name
-            unix_socket=f"/cloudsql/{os.environ.get('DB_CONNECTION_NAME')}" # Replace with your Cloud SQL connection name
-        )
+        if os.environ.get("GAE_ENV", "").startswith("standard") or os.environ.get("K_SERVICE"):
+            # Running on Cloud Run or App Engine
+            return mysql.connector.connect(
+                user=os.environ.get('DB_USER'),  # Replace with your MySQL username
+                password=os.environ.get('DB_PASS'),  # Replace with your MySQL password
+                database=os.environ.get('DB_NAME'),  # Replace with your database name
+                unix_socket=f"/cloudsql/{os.environ.get('DB_CONNECTION_NAME')}" # Replace with your Cloud SQL connection name
+            )
     except Error as err:
         print(f"Error: {err}")
         return None
