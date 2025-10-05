5744a30ebdf706bfcfbb820ff86ded9e41b1c66d
Chybozz <martinsonuchukwuchimdinyerem.com>
2025-10-05 11:05:02 +0100

    Latest Commit

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import mysql.connector
 
 def get_db_connection():
     return mysql.connector.connect(
-        host='stallionroutes-db.cnuw08cmoyx3.eu-north-1.rds.amazonaws.com',
+        host='stallionroutes-id.cnuw08cmoyx3.eu-north-1.rds.amazonaws.com',
         user='stallion_user',
         password='stallionroutes123!',
-        database='stallionroutes-db'  # Replace with your database name
+        database='stallionroutes_db'  # Replace with your database name
     )
